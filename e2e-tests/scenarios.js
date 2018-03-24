ab11bbfb81b865d4d556ab7292ae4a1c29bd9fdc
alexloev <alexloev1988@gmail.com>
2018-03-24 16:54:46 +0300

    test myaccount page

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -2,6 +2,17 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
+var origFn = browser.driver.controlFlow().execute;
+browser.driver.controlFlow().execute = function () {
+  var args = arguments;
+  // queue 100ms wait
+  origFn.call(browser.driver.controlFlow(), function () {
+    return protractor.promise.delayed(100);   // here we can adjust the execution speed
+  });
+  return origFn.apply(browser.driver.controlFlow(), args);
+  // console.log('something');
+};
+
 describe('my app', function() {
   describe('should highlighted btn-primary depends of routes', function() {
     it('highlighted "Список"', function() {
@@ -22,17 +33,33 @@ describe('my app', function() {
   });
 
   describe('page /myaccount', function() {
-    before(function() {
+    //beforeEach(origFn);
+    beforeAll(function() {
       browser.get('index.html#!/myaccount');
     });
     it('should have form', function() {
       expect(element(by.name('myaccountForm')).isPresent()).toBe(true);
     });
-    it('should have required fields', function() {
+    it('should disable submit button cause of required fields', function() {
       expect(element(by.name('myaccountSubmit')).isEnabled()).toBe(false);
     });
-  });
+    it('fill the controls', function() {
+      var username = element(by.name('username'));
+      expect(username.isPresent()).toBe(true);
+      username.sendKeys('test user');
+      var useremail = element(by.name('useremail'));
+      expect(useremail.isPresent()).toBe(true);
+      useremail.sendKeys('test@test.com');
+    });
+    it('should enabled submit button cause of filling required fields', function() {
+      expect(element(by.name('myaccountSubmit')).isEnabled()).toBe(true);
+    });
 
+  });
+  
+  describe('showlist add pokemon to cart', {
+    
+  });
 
   // describe('view2', function() {
 
